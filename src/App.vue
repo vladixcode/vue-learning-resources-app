3ab686648536c8eef04e18b243181b2dedb30fe7
Vladimir Ilic <vladimir.ilic101@gmail.com>
2024-02-08 15:00:19 +0100

    Handling and rendering dummy data

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
 
-<template><div>Plain initial setup works</div></template>
+import LearningResource from '@/components/LearningResources/LearningResource.vue'
+
+// Dummy data
+const storedResources = reactive([
+  {
+    id: 1,
+    title: 'Official Guide',
+    description: 'The official Vue.js documentation',
+    link: 'https://vuejs.org/',
+  },
+  {
+    id: 2,
+    title: 'Google',
+    description: 'Learn to google...',
+    link: 'https://www.google.com/',
+  },
+])
+</script>
+
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
+  </ul>
+</template>
 
 <style scoped></style>
---
+++ src/components/LearningResources/LearningResource.vue
@@ -0,0 +1,33 @@
+<script setup>
+// import { defineProps } from 'vue'
+
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link">View Resource</a>
+    </nav>
+  </li>
+</template>
