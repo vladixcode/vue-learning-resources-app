e39f1961941702317242b880601c8e9a76820d16
Vladimir Ilic <vladimir.ilic101@gmail.com>
2024-02-09 14:59:25 +0100

    Add teleport to BaseDialog component

--- src/components/UI/BaseDialog.vue
+++ src/components/UI/BaseDialog.vue
@@ -10,22 +10,24 @@ defineEmits(['close'])
 </script>
 
 <template>
-  <div class="gray-overlay" @click="$emit('close')"></div>
-  <dialog open>
-    <header>
-      <slot name="header">
-        <h2>{{ title }}</h2>
-      </slot>
-    </header>
-    <section>
-      <slot></slot>
-    </section>
-    <menu>
-      <slot name="actions">
-        <base-button @click="$emit('close')">Close</base-button>
-      </slot>
-    </menu>
-  </dialog>
+  <teleport to="body">
+    <div class="gray-overlay" @click="$emit('close')"></div>
+    <dialog open>
+      <header>
+        <slot name="header">
+          <h2>{{ title }}</h2>
+        </slot>
+      </header>
+      <section>
+        <slot></slot>
+      </section>
+      <menu>
+        <slot name="actions">
+          <base-button @click="$emit('close')">Close</base-button>
+        </slot>
+      </menu>
+    </dialog>
+  </teleport>
 </template>
 
 <style scoped>
